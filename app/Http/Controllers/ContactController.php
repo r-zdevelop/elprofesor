6663fb2dde4281717644bec2fefe9e2ac1b5b71a
Regynald Zambrano <regynald.zambrano@gmail.com>
2025-08-29 21:14:32 -0500

    write the right email to send

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -17,8 +17,8 @@ class ContactController extends Controller
 
         // Example: send email
         Mail::raw("Message from: {$data['name']} ({$data['email']})\n\n{$data['message']}", function($msg) use ($data) {
-            $msg->to('your@email.com')
-                ->subject('New Contact Form Message');
+            $msg->to('regynald.zambrano@gmail.com')
+                ->subject('New Contact Form Message from elprofesor.dev');
         });
 
         return back()->with('success', 'Your message has been sent successfully!');
